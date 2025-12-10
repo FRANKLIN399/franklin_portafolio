d31f0a3bc190040c2b44d6ea8c64f18042bcb56a
Franklin Esteban <franklinesteban2408@gmail.com>
2025-12-10 01:12:51 -0500

    config: actualizar la ruta base en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/franklin_portafolio/',
   plugins: [react()],
-  base: "./",
 })
 
